4c89dcc77f27a31ea5028e63bcd92b32446e7a69
lch275 <chanho.lee0817@gmail.com>
2026-06-15 21:39:31 +0900

    feat: db to csv 예제 추가

---
+++ src/main/java/com/lch275/batch/job/order/OrderExportJobConfig.java
@@ -0,0 +1,89 @@
+package com.lch275.batch.job.order;
+
+import com.lch275.batch.job.order.chunck.OrderLoadItemProcessor;
+import com.lch275.batch.job.order.chunck.dto.OrderDTO;
+import com.lch275.batch.job.order.domain.OrderRepository;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.WritableResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class OrderExportJobConfig {
+    private static final int CHUNCK_SIZE = 10000;
+    private final JobRepository jobRepository;
+    private final EntityManagerFactory entityManagerFactory;
+    private final PlatformTransactionManager platformTransactionManager;
+    private final OrderRepository orderRepository;
+
+    @Bean
+    public Job orderExportJob() {
+        return new JobBuilder("orderExportJob", jobRepository)
+                .start(orderExportStep())
+                .build();
+    }
+
+    @Bean
+    public Step orderExportStep() {
+        return new StepBuilder("orderExportStep", jobRepository)
+                .<OrderDTO, OrderDTO>chunk(CHUNCK_SIZE, platformTransactionManager)
+                .reader(jpaPagingItemReader())
+                .writer(flatFileItemWriter())
+                .allowStartIfComplete(true)
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<OrderDTO> jpaPagingItemReader() {
+        return new JpaPagingItemReaderBuilder<OrderDTO>()
+                .name("jpaPagingItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(CHUNCK_SIZE)
+                .queryString("SELECT o FROM Order o")
+                .build();
+    }
+
+    @Bean
+    public FlatFileItemWriter<OrderDTO> flatFileItemWriter() {
+
+        StringBuilder sb = new StringBuilder();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        sb.append("output/orders");
+        sb.append("_");
+        sb.append(LocalDate.now(ZoneId.of("Asia/Seoul")).format(formatter));
+        sb.append(".csv");
+
+        return new FlatFileItemWriterBuilder<OrderDTO>()
+                .name("flatFileItemWriter")
+                .resource(new FileSystemResource(sb.toString()))
+                .delimited()
+                    .delimiter(",")
+                    .names("orderId", "productName", "quantity", "price", "orderDate")
+                .build();
+    }
+}
